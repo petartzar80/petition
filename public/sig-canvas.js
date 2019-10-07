5123fd10bd0ccc694d647ad8ca703461288f6254
Petar Tzar <petartzar80@gmail.com>
2019-10-07 09:49:35 +0200

    getting value from txt input & canvas

--- public/sig-canvas.js
+++ public/sig-canvas.js
@@ -4,7 +4,6 @@ let x = 0;
 let y = 0;
 
 const sigCanvas = document.getElementById("sigCanvas");
-console.log(sigCanvas);
 const context = sigCanvas.getContext("2d");
 
 // The x and y offset of the canvas from the edge of the page
@@ -43,3 +42,19 @@ function drawLine(context, x1, y1, x2, y2) {
     context.stroke();
     context.closePath();
 }
+
+// submit button
+
+const button = $("button");
+const first = document.getElementById("first");
+console.log(first);
+const last = document.getElementById("last");
+console.log(last);
+
+button.on("click", function() {
+    console.log("clicking");
+    console.log("first: ", first.value);
+    console.log("last: ", last.value);
+    let canvasValue = sigCanvas.toDataURL();
+    console.log("signature: ", canvasValue);
+});

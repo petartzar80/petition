c4cd83f6ed0605b038ecb5e1da55b3ab3588dffa
Petar Tzar <petartzar80@gmail.com>
2019-10-14 07:05:38 +0200

    Basic hipster styling finally done

--- public/sig-canvas.js
+++ public/sig-canvas.js
@@ -35,8 +35,8 @@ window.addEventListener("mouseup", e => {
 
 function drawLine(context, x1, y1, x2, y2) {
     context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = 1;
+    context.strokeStyle = "yellow";
+    context.lineWidth = 3;
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
